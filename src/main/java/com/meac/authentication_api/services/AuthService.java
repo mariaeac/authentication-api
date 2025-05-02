9e1c3dd7f252eac86cbe558f685ae6a81837b97f
Maria Eduarda <meac21kj@gmail.com>
2025-05-02 00:05:05 -0300

    feat: Authentication Service

---
+++ src/main/java/com/meac/authentication_api/services/AuthService.java
@@ -0,0 +1,23 @@
+package com.meac.authentication_api.services;
+import com.meac.authentication_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+
+
+}
